3e1f4c00c09b4a5aaba41c75a0645f423cd06797
shabeer m <shabeer.m9999@gmail.com>
2022-03-23 12:35:06 +0530

    Second

--- lib/screens/Settings_screen.dart
+++ lib/screens/Settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../custom/Settings_custom.dart';
 
@@ -10,11 +11,122 @@ class Settings_screen extends StatefulWidget {
 }
 
 class _Settings_screenState extends State<Settings_screen> {
-  bool isSwitch = true;
+  bool notify = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getSwitchValues();
+  }
+
+  getSwitchValues() async {
+    notify = await getSwitchState();
+    setState(() {});
+  }
+
+  Future<bool> saveSwitchState(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("switchState", value);
+    return prefs.setBool("switchState", value);
+  }
+
+  Future<bool> getSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? notify = prefs.getBool("switchState");
+
+    return notify ?? true;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return
+//       backgroundColor: Colors.black,
+//       appBar: AppBar(
+//         backgroundColor: Colors.black,
+//         centerTitle: true,
+//         elevation: 0,
+//         title: const Text(
+//           "Settings",
+//           style: TextStyle(),
+//         ),
+//         leading: GestureDetector(
+//           onTap: () {
+//             Navigator.pop(context);
+//           },
+//           child: const Icon(
+//             Icons.arrow_back_outlined,
+//             size: 30,
+//           ),
+//         ),
+//       ),
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           image: DecorationImage(
+//               image: AssetImage('assets/download (1).jpg'), fit: BoxFit.cover),
+//         ),
+//         child: Column(
+//           children: [
+//             const SizedBox(
+//               height: 10,
+//             ),
+//             ListTile(
+//               iconColor: Colors.white,
+//               dense: true,
+//               title: const Text("Notifications",
+//                   style: TextStyle(
+//                       fontSize: 25, color: Color.fromARGB(255, 231, 238, 243))),
+//               horizontalTitleGap: 2,
+//               trailing: Switch(
+//                   value: isSwitch,
+//                   inactiveThumbColor: Color.fromARGB(255, 81, 91, 100),
+//                   inactiveTrackColor: Colors.white,
+//                   activeTrackColor: Color.fromARGB(255, 246, 248, 250),
+//                   activeColor: Color.fromARGB(255, 30, 75, 138),
+//                   onChanged: (val) {
+//                     setState(() {
+//                       isSwitch = val;
+//                     });
+//                   }),
+//               contentPadding: const EdgeInsets.fromLTRB(19, 0, 7, 0),
+//               leading: const Icon(
+//                 Icons.notification_add,
+//                 size: 22,
+//               ),
+//             ),
+//             Settings_custom(
+//               icon: const Icon(
+//                 Icons.lock_outlined,
+//                 size: 22,
+//               ),
+//               left: 16,
+//               gap: 5,
+//               titletext: "Privacy Policy",
+//             ),
+//             Settings_custom(
+//                 icon: const Icon(
+//                   Icons.panorama_wide_angle_outlined,
+//                   size: 22,
+//                 ),
+//                 left: 16,
+//                 gap: 5,
+//                 titletext: "Terms & Conditions"),
+//             Settings_custom(
+//               icon: const Icon(Icons.share_outlined, size: 22),
+//               titletext: "Share The App",
+//               left: 19,
+//             ),
+//             Settings_custom(
+//               icon: const Icon(Icons.messenger_outline, size: 22),
+//               titletext: "About",
+//               left: 19,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+        Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -34,69 +146,73 @@ class _Settings_screenState extends State<Settings_screen> {
           ),
         ),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/download (1).jpg'), fit: BoxFit.cover),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
+      body: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.notifications, color: Colors.greenAccent),
+            title: const Text(
+              "Notifications",
+              style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-            ListTile(
-              iconColor: Colors.white,
-              dense: true,
-              title: const Text("Notifications",
-                  style: TextStyle(
-                      fontSize: 25, color: Color.fromARGB(255, 231, 238, 243))),
-              horizontalTitleGap: 2,
-              trailing: Switch(
-                  value: isSwitch,
-                  inactiveThumbColor: Color.fromARGB(255, 81, 91, 100),
-                  inactiveTrackColor: Colors.white,
-                  activeTrackColor: Color.fromARGB(255, 246, 248, 250),
-                  activeColor: Color.fromARGB(255, 30, 75, 138),
-                  onChanged: (val) {
-                    setState(() {
-                      isSwitch = val;
-                    });
-                  }),
-              contentPadding: const EdgeInsets.fromLTRB(19, 0, 7, 0),
-              leading: const Icon(
-                Icons.notification_add,
-                size: 22,
-              ),
+            trailing: IconButton(
+                icon: Switch(
+                    value: notify,
+                    inactiveThumbColor: Colors.greenAccent,
+                    inactiveTrackColor: Colors.white,
+                    activeTrackColor: Color.fromARGB(255, 246, 248, 250),
+                    activeColor: Color.fromARGB(255, 30, 75, 138),
+                    onChanged: (bool value) {
+                      setState(() {
+                        notify = value;
+                        saveSwitchState(value);
+                      });
+                    }),
+                onPressed: () {}),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.share,
+              color: Colors.greenAccent,
             ),
-            Settings_custom(
-              icon: const Icon(
-                Icons.lock_outlined,
-                size: 22,
-              ),
-              left: 16,
-              gap: 5,
-              titletext: "Privacy Policy",
+            onTap: () {},
+            title: Text(
+              'Share The App',
+              style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-            Settings_custom(
-                icon: const Icon(
-                  Icons.panorama_wide_angle_outlined,
-                  size: 22,
-                ),
-                left: 16,
-                gap: 5,
-                titletext: "Terms & Conditions"),
-            Settings_custom(
-              icon: const Icon(Icons.share_outlined, size: 22),
-              titletext: "Share The App",
-              left: 19,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.lock,
+              color: Colors.greenAccent,
             ),
-            Settings_custom(
-              icon: const Icon(Icons.messenger_outline, size: 22),
-              titletext: "About",
-              left: 19,
+            onTap: () {},
+            title: Text(
+              'Privacy Policies',
+              style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-          ],
-        ),
+          ),
+          const ListTile(
+            leading: Icon(
+              Icons.receipt,
+              color: Colors.greenAccent,
+            ),
+            title: Text(
+              'Terms and Conditions',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.info,
+              color: Colors.greenAccent,
+            ),
+            title: Text(
+              'About',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 250),
+        ],
       ),
     );
   }
